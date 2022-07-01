e78ccf240806261e9427694a9057eeefd42bba66
omondi-alex <omondialex3517@gmail.com>
2022-07-02 01:17:50 +0300

    prints all possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main -prints all possible different comb of three digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1 = 48;
+	int num2, num3;
+	int comb = 44;
+
+	while (num1 <= 57)
+	{
+		num2 = num1 + 1;
+		while (num2 <= 57)
+		{
+			num3 = num2 + 1;
+			while (num3 <= 57)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+
+				if (num1 != 55 || num2 != 56 || num3 != 57)
+				{
+					putchar(comb);
+					putchar(32);
+				}
+				num3 += 1;
+			}
+			num2 += 1;
+		}
+		num1 += 1;
+	}
+	putchar('\n');
+	return (0);
+}
