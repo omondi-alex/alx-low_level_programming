a05d24bea3829854f7e0ab149119195ebb120fd2
omondi-alex <omondialex3517@gmail.com>
2022-07-06 14:27:54 +0300

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,25 +2,24 @@
 
 /**
  * main - computes and prints the sum of all the multiples
- * of 3 or 5 below 1024
- * Return: Always 0 (Success)
+ * of 3 or 5 below 1024. Done the Felix Obianozie way
+ * Return: 0
  */
+
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
+	int sum;
 	int i;
 
-	sum3 = 0;
-	sum5 = 0;
 	sum = 0;
 
 	for (i = 0; i < 1024; ++i)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
+			sum = sum + i;
 		}
 	}
+	printf("%i\n", sum);
+	return (0);
+}
