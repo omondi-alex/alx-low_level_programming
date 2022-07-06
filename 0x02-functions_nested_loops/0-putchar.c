76c740992625324f49e470db2b6d3ed72b11163c
omondi-alex <omondialex3517@gmail.com>
2022-07-06 09:10:31 +0300

    prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -16,4 +16,26 @@ int main(void)
 	}
 	_putchar('\n');
 	return (0);
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
